cc6fd9964d85dd4feb92858e3258a8faad68dbf6
Grha Gandana P <grhagandanap@gmail.com>
2026-02-14 07:55:43 +0700

    init monorepo

---
+++ apps/admin/src/integrations/tanstack-query/root-provider.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useState } from 'react'
+
+export function TanStackQueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  }))
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  )
+}
+
+export function getContext() {
+  const queryClient = new QueryClient()
+  return {
+    queryClient,
+  }
+}
