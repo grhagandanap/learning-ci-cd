931cae5c4a8635bd72222e6cf512bd490d1695c3
Grha Gandana P <grhagandanap@gmail.com>
2026-02-16 17:51:05 +0700

    adding nginx setup

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -29,11 +29,11 @@ const config = defineConfig({
 		viteReact(),
 	],
 	preview: {
-		host: '0.0.0.0',
+		host: "0.0.0.0",
 		port: 3000, // or 4000 for admin
 		strictPort: true,
-		allowedHosts: true // Allow all hosts
-	}
+		allowedHosts: true, // Allow all hosts
+	},
 });
 
 export default config;
